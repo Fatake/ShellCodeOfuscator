4f6d9b820d4d0ce4bb4278070a97e4540df10f2a
Fatake <fatake@fatake.com>
2023-04-26 17:24:57 -0600

    Adding aes

--- coder/coder.go
+++ coder/coder.go
@@ -1,7 +1,10 @@
 package coder
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base32"
+	"encoding/hex"
 	"strings"
 )
 
@@ -18,4 +21,47 @@ func Base32CustomEncoder(codigo string) string {
 	pay := base32.StdEncoding.EncodeToString([]byte(codigo))
 	rep := strings.Replace(pay, "Q", "[/___[*]]", -1)
 	return rep
+}
+
+func HexEncode(code string) string {
+	contenido := hex.EncodeToString([]byte(code))
+	return contenido
+}
+
+func newAead(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	return aead, nil
+}
+
+func AESEncrypt(plain []byte, key, nonce []byte) []byte {
+	aead, err := newAead(key)
+	if err != nil {
+		println(err.Error())
+		return nil
+	}
+
+	return aead.Seal(plain[:0], nonce, plain, nil)
+}
+
+func AESDecrypt(cipher []byte, key, nonce []byte) []byte {
+	aead, err := newAead(key)
+	if err != nil {
+		println(err.Error())
+		return nil
+	}
+
+	output, err := aead.Open(cipher[:0], nonce, cipher, nil)
+	if err != nil {
+		println(err.Error())
+		return nil
+	}
+
+	return output
 }
