528c4388315077000e75a6d798fdc393f7a735fd
Fatake <fatake@fatake.com>
2023-04-26 17:37:33 -0600

    update

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Fatake/ShellCodeOfuscator/coder"
 	"github.com/fatih/color"
@@ -32,7 +35,7 @@ func opciones() *FlagsType {
 }
 
 func main() {
-	color.Green(banner)
+	color.Cyan(banner)
 	menu := opciones()
 	if menu.inputFile == "" {
 		log.Fatal("[!] Requiere binario, user -h para ver la ayuda")
@@ -43,7 +46,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// se cifra la carga parametrizada
+	////
+	// XOR Part
+	////
+	color.Blue("[i] Realizando proceso de cifrado Xor")
 	cifrar := coder.XorEncoder(dataFile, 60)
 
 	// obs es la variable que contiene el resultado final de la obfuscacion
@@ -51,5 +57,18 @@ func main() {
 
 	os.WriteFile("obs.txt", []byte(obs), 0777)
 	color.Yellow("[+] Archivo Ofuscado Exitosamente")
+
+	////
+	// AES Part
+	////
+	color.Blue("[i] Realizando proceso de cifrado AES")
+	key := make([]byte, 32)
+	nonce := make([]byte, 12)
+
+	rand.Read(key)
+	rand.Read(nonce)
+	time.Sleep(2 * time.Second)
+	color.Green("[+] Key: " + base64.StdEncoding.EncodeToString(key))
+	color.Green("[+] nonce: " + base64.StdEncoding.EncodeToString(nonce))
 
 }
